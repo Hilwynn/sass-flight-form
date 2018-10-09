4401263f0939d3a1189af02945717456aa69bb2b
Anna Erkers <31205429+Hilwynn@users.noreply.github.com>
2018-10-09 20:06:15 +0200

    Assignment done - no stretch goals

--- code/src/components/app.js
+++ code/src/components/app.js
@@ -4,8 +4,72 @@ class App extends React.Component {
 
   render() {
     return (
-      <div>
-        Find me in src/app.js!
+      <div className="wrapper">
+        <div className="booking-container">
+          <div className="tabs">
+            <div className="tab">
+              <span className="tab-icon">
+                <i className="fas fa-ticket-alt" />
+              </span>
+              <span className="tab-label">Flyg</span>
+            </div>
+            <div className="tab">
+              <span className="tab-icon">
+                <i className="fas fa-h-square" />
+              </span>
+              <span className="tab-label">Flyg + Hotell</span>
+            </div>
+            <div className="tab">
+              <span className="tab-icon">
+                <i className="fas fa-car" />
+              </span>
+              <span className="tab-label">Hyrbil</span>
+            </div>
+            <div className="tab">
+              <span className="tab-icon">
+                <i className="fas fa-hotel" />
+              </span>
+              <span className="tab-label">Hotell</span>
+            </div>
+            <div className="tab">
+              <span className="tab-icon">
+                <i className="fas fa-medal" />
+              </span>
+              <span className="tab-label">Reward</span>
+            </div>
+          </div>
+          <div className="form">
+            <div className="airport-select-field">
+              <div className="airport-select-wrapper">
+                <label htmlFor="departure">
+                  <span className="airport-select-label">Fly from</span>
+                  <input type="text" id="departure" />
+                  <span className="airport-select-icon">
+                    <i className="fas fa-plane-departure" />
+                  </span>
+                  <span className="airport-select-border" />
+                </label>
+              </div>
+              <div className="airport-select-wrapper">
+                <label htmlFor="destination">
+                  <span className="airport-select-label">Fly to</span>
+                  <input type="text" id="destination" />
+                  <span className="airport-select-border" />
+                  <span className="airport-select-icon">
+                    <i className="fas fa-plane-arrival" />
+                  </span>
+                </label>
+              </div>
+            </div>
+            <p>
+              <a href="#">
+                Stockholm - Arlanda
+              </a>
+               ?
+            </p>
+            <button type="submit">Sök och beställ</button>
+          </div>
+        </div>
       </div>
     )
   }
